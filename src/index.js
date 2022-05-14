4e33520cd526a86d5619c37b95eaaa28ea20c838
Arya Buddha <40647186+AryaBuddha@users.noreply.github.com>
2022-05-14 11:21:14 -0400

    Fixed yarn dev

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import {
   ApolloServer
 } from 'apollo-server';
+
 import typeDefs from "./schema/index.js";
 import resolvers from "./resolvers/index.js";
 
 
-
-
-
-
-//const typeDefs = [Query, volunteerDef]
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-// Launch the server
+
 
 server.listen().then(({
   url
